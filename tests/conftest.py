5c77b40f807199ddcfa2e23529445a937e375801
Francisco Abarzua <git.id@frabarz.cl>
2024-03-01 19:48:44 -0300

    updates tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,6 +12,8 @@ from random import randint
 
 import httpx
 import pytest
+from fastapi.requests import Request
+from fastapi.responses import JSONResponse, PlainTextResponse
 
 import logiclayer as ll
 
@@ -25,21 +27,48 @@ def route_check():
     res = httpx.get("http://clients3.google.com/generate_204")
     return (res.status_code == 204) and (res.headers.get("Content-Length") == 0)
 
+
 def route_status():
     return {"status": "ok", "software": "LogicLayer", "version": ll.__version__}
 
 
 class EchoModule(ll.LogicLayerModule):
-    def __init__(self, **kwargs):
+    def __init__(self, label: str, **kwargs):
         super().__init__(**kwargs)
         self.random = randint(0, 9)
+        self.label = label
+
+    @ll.healthcheck
+    async def healthcheck(self):
+        print("HEALTHCHECK")
+        return True
+
+    @ll.on_startup
+    async def event_startup(self):
+        print("STARTUP", self.label)
+
+    @ll.on_shutdown
+    def event_shutdown(self):
+        print("SHUTDOWN", self.label)
+
+    @ll.route("GET", "/", name="Index route for base")
+    async def route_index(self):
+        return {"label": self.label}
+
+    @ll.route("GET", "/file.{ext}")
+    def route_file(self, ext: str):
+        return PlainTextResponse(f"Label: {self.label}\nExtension: {ext}")
+
+    @ll.route("GET", "/error")
+    def route_error(self):
+        raise ValueError(self.label)
 
     @ll.route("GET", "/random")
     async def route_random(self):
         return {"random": self.random}
 
     @ll.route("GET", "/empty")
-    def route_empty():
+    def route_empty(self):
         return {}
 
     @ll.route("GET", "/number")
@@ -54,10 +83,22 @@ class EchoModule(ll.LogicLayerModule):
     async def route_body(self, body: BodySchema):
         return body.value
 
+    @ll.exception_handler(ValueError)
+    def exc_valueerror(self, request: "Request", exc: "ValueError"):
+        return JSONResponse(
+            {
+                "label": self.label,
+                "route": "exc_valueerror",
+                "request": str(request.url),
+                "exc": str(exc),
+            },
+            status_code=504,
+        )
+
 
 @pytest.fixture
 def layer():
-    echo = EchoModule()
+    echo = EchoModule("Hello")
 
     layer = ll.LogicLayer()
     layer.add_check(route_check)
--- tests/test_logiclayer.py
+++ tests/test_logiclayer.py
@@ -3,13 +3,6 @@ from fastapi.testclient import TestClient
 import logiclayer as ll
 
 
-def test_route_check(layer: ll.LogicLayer):
-    with TestClient(app=layer) as client:
-        res = client.get("/_health")
-
-    assert res.status_code == 204
-    assert res.text == ""
-
 def test_route_status(layer: ll.LogicLayer):
     with TestClient(app=layer) as client:
         res = client.get("/")
@@ -21,6 +14,44 @@ def test_route_status(layer: ll.LogicLayer):
         "version": ll.__version__,
     }
 
+
+def test_route_check(layer: ll.LogicLayer):
+    with TestClient(app=layer) as client:
+        res = client.get("/_health")
+
+    assert res.status_code == 204
+    assert res.text == ""
+
+
+def test_route_index(layer: ll.LogicLayer):
+    with TestClient(app=layer) as client:
+        res = client.get("/echo/")
+
+    assert res.status_code == 200
+    assert res.json() == {"label": "Hello"}
+
+
+def test_route_file(layer: ll.LogicLayer):
+    with TestClient(app=layer) as client:
+        res = client.get("/echo/file.asdf")
+
+    assert res.status_code == 200
+    assert res.text == "Label: Hello\nExtension: asdf"
+
+
+def test_route_error(layer: ll.LogicLayer):
+    with TestClient(app=layer, base_url="http://demoserver/") as client:
+        res = client.get("/echo/error")
+
+    assert res.status_code == 504
+    assert res.json() == {
+        "label": "Hello",
+        "route": "exc_valueerror",
+        "request": "http://demoserver/echo/error",
+        "exc": "Hello",
+    }
+
+
 def test_route_random(layer: ll.LogicLayer):
     with TestClient(app=layer) as client:
         res1 = client.get("/echo/random")
@@ -30,6 +61,7 @@ def test_route_random(layer: ll.LogicLayer):
     assert res2.status_code == 200
     assert res1.json() == res2.json()
 
+
 def test_route_empty(layer: ll.LogicLayer):
     with TestClient(app=layer) as client:
         res = client.get("/echo/empty")
@@ -37,12 +69,14 @@ def test_route_empty(layer: ll.LogicLayer):
     assert res.status_code == 200
     assert res.json() == {}
 
+
 def test_route_query(layer: ll.LogicLayer):
     with TestClient(app=layer) as client:
         res = client.get("/echo/number?value=10")
 
     assert res.status_code == 200
     assert res.json() == {"number": 10}
+
 
 def test_route_path(layer: ll.LogicLayer):
     with TestClient(app=layer) as client:

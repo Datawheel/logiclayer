820d7b59e6207d883b72e873a3be66298b0e7d43
Francisco Abarzua <git.id@frabarz.cl>
2024-04-19 13:20:10 -0400

    v0.3.3

--- logiclayer/__init__.py
+++ logiclayer/__init__.py
@@ -1,9 +1,10 @@
 """LogicLayer module."""
 
-from .auth import AuthProvider, AuthToken, AuthTokenType
-from .decorators import exception_handler, healthcheck, on_shutdown, on_startup, route
-from .logiclayer import LogicLayer
-from .module import LogicLayerModule, ModuleStatus
+__title__ = "logiclayer"
+__description__ = (
+    "A framework to quickly compose and use multiple functionalities as endpoints."
+)
+__version__ = "0.3.3"
 
 __all__ = (
     "AuthProvider",
@@ -19,4 +20,7 @@ __all__ = (
     "route",
 )
 
-__version__ = "0.3.2"
+from .auth import AuthProvider, AuthToken, AuthTokenType
+from .decorators import exception_handler, healthcheck, on_shutdown, on_startup, route
+from .logiclayer import LogicLayer
+from .module import LogicLayerModule, ModuleStatus
